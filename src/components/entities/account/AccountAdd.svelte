97de6a4a94ab51f2b85a91eaecdde968c996b0e5
Magomedsoltan Kasumov <77916046+kenzWeb@users.noreply.github.com>
2024-10-16 02:45:49 +0300

    Fix reset inputs

--- src/components/entities/account/AccountAdd.svelte
+++ src/components/entities/account/AccountAdd.svelte
@@ -2,13 +2,13 @@
 import * as Dialog from '$components/ui/dialog'
 import * as m from '$m'
 import Default from '$src/components/ui/accountModal/Default.svelte'
-import { isSuccessModal } from '$src/utils/functions/isSuccessModal'
+import { accountModal } from '$src/utils/functions/accountModal'
 import { accountIsSuccess } from '$stores/store'
 </script>
 
 <Dialog.Root>
   <Dialog.Trigger
-    on:click={isSuccessModal}
+    on:click={accountModal}
     class="inline-flex h-10 items-center justify-center rounded-md border border-green-500 bg-transparent px-4 py-2 text-sm font-medium text-primary-foreground ring-offset-background transition-colors hover:bg-green-500/90 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
   >
     {`+ ${m.addAccount()}`}
