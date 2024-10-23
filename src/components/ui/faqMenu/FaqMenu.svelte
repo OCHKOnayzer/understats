459ce785e36886261cb999b47875bdc16e2a43f9
OCHKOnayzer <popsof@yandex.ru>
2024-10-23 17:22:18 +0400

    faq page fix

---
+++ src/components/ui/faqMenu/FaqMenu.svelte
@@ -0,0 +1,19 @@
+<script>
+import { t } from 'svelte-i18n'
+import FaqMenuElements from './faqMenuElements.svelte'
+</script>
+
+<div class="faqMenu">
+  <FaqMenuElements />
+</div>
+
+<style>
+.faqMenu {
+  width: 25%;
+  border-radius: 5px;
+  background-color: #171b26;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ src/components/ui/faqMenu/faqConteiner.svelte
@@ -0,0 +1,24 @@
+<script>
+import FaqMenu from './FaqMenu.svelte'
+</script>
+
+<div class="faqSection">
+  <FaqMenu />
+  <div class="faqChapter"></div>
+</div>
+
+<style>
+.faqSection {
+  width: 98%;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  height: 82vh;
+}
+
+.faqChapter {
+  width: 74%;
+  border-radius: 5px;
+  background-color: #252935;
+}
+</style>
