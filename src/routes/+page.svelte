02b281a588ea7e92faf37f801d4d95292ad3d32b
Magomedsoltan Kasumov <77916046+kenzWeb@users.noreply.github.com>
2024-10-27 18:23:19 +0300

    Added accounts endpoint

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,6 @@
 <script>
-import Account from '$components/widgets/account/Account.svelte'
-import Filter from '$components/widgets/filter/Filter.svelte'
-import SortContainer from '$components/widgets/sortContainer/SortContainer.svelte'
-import Table from '$components/widgets/table/Table.svelte'
 </script>
 
 <div>
-  <SortContainer />
-  <Account />
-  <Filter />
-  <Table />
+  hello
 </div>
