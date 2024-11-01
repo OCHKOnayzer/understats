fa3c753c401ebdf257184dbd3b2fa72c4d0b952d
Magomedsoltan Kasumov <77916046+kenzWeb@users.noreply.github.com>
2024-11-01 23:57:16 +0300

    Added filter

---
+++ src/routes/stats/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+import Container from '$src/components/providers/container/Container.svelte'
+import Header from '$src/components/widgets/stats/header/Header.svelte'
+import StatsMenu from '$src/components/widgets/stats/StatsMenu/StatsMenu.svelte'
+</script>
+
+<div>
+  <Container>
+    <Header />
+    <StatsMenu />
+  </Container>
+</div>
