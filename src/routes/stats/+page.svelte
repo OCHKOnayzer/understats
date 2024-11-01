c059888d05fcacb334a291d484b970ce14ea37bf
Magomedsoltan Kasumov <77916046+kenzWeb@users.noreply.github.com>
2024-11-02 01:06:50 +0300

    Added bets overview section

--- src/routes/stats/+page.svelte
+++ src/routes/stats/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 import Container from '$src/components/providers/container/Container.svelte'
+import BetsOverview from '$src/components/widgets/stats/BetsOverview/BetsOverview.svelte'
 import Header from '$src/components/widgets/stats/header/Header.svelte'
 import StatsMenu from '$src/components/widgets/stats/StatsMenu/StatsMenu.svelte'
 </script>
@@ -8,5 +9,6 @@ import StatsMenu from '$src/components/widgets/stats/StatsMenu/StatsMenu.svelte'
   <Container>
     <Header />
     <StatsMenu />
+    <BetsOverview />
   </Container>
 </div>
