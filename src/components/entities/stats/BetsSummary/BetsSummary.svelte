1fcf4bb40b4573e9fb188453db4b2306669835f5
nkz9 <nkz9>
2024-11-02 05:39:43 +0300

    eslint format

--- src/components/entities/stats/BetsSummary/BetsSummary.svelte
+++ src/components/entities/stats/BetsSummary/BetsSummary.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-import TotalBet from '../TotalBet/TotalBet.svelte'
+import TotalBet from '../TotalBet/TotalBet.svelte';
 </script>
 
-<div class="flex items-center justify-between w-[80%] gap-4">
-  <TotalBet title="356 ставок" sum="На сумму 158 000 Р"/>
-	<TotalBet title="356 выигрышей" sum="На сумму 158 000 Р"/>
-	<TotalBet title="356 проигрышей" sum="На сумму 158 000 Р"/>
+<div class="flex w-[80%] items-center justify-between gap-4">
+	<TotalBet title="356 ставок" sum="На сумму 158 000 Р" />
+	<TotalBet title="356 выигрышей" sum="На сумму 158 000 Р" />
+	<TotalBet title="356 проигрышей" sum="На сумму 158 000 Р" />
 </div>
