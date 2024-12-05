1442d5665c35c6c6c4be06a7889e820eec7850e6
OCHKOnayzer <popsof@yandex.ru>
2024-12-05 17:26:22 +0400

    OCHKOnayer tailwind config upd

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,6 +20,7 @@ const config: Config = {
 				ring: 'hsl(var(--ring) / <alpha-value>)',
 				background: 'hsl(var(--background) / <alpha-value>)',
 				foreground: 'hsl(var(--foreground) / <alpha-value>)',
+				activeElements: 'hsl(var(--active-elements) / <alpha-value>)',
 				primary: {
 					DEFAULT: 'hsl(var(--primary) / <alpha-value>)',
 					foreground: 'hsl(var(--primary-foreground) / <alpha-value>)'
