56d760361e5c13dd59cfb0aedbf4f69457e173a9
nkz9 <nkz9>
2024-12-06 00:34:27 +0300

    deploy

--- svelte.config.ts
+++ svelte.config.ts
@@ -1,7 +1,9 @@
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const config = {
+import type { Config } from '@sveltejs/kit';
+
+const config: Config = {
 	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter(),
