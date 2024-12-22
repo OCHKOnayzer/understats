4d28310533f1f8a595ec88f9736e18abbf1fe558
nkz9 <nkz9>
2024-12-22 11:42:49 +0300

    langs, chat

--- src/components/ui/menu/chatMenu.svelte
+++ src/components/ui/menu/chatMenu.svelte
@@ -2,6 +2,8 @@
 import { t } from 'svelte-i18n';
 import { onMount } from 'svelte';
 
+import { langSel } from '$stores/HeaderStores.ts';
+
 let isChatOpen = false;
 
 onMount(() => {
@@ -14,18 +16,72 @@ onMount(() => {
 			isChatOpen = false;
 		};
 	}
+
+	if (window.jivo_onOpen) {
+		window.jivo_onOpen = () => {
+			isChatOpen = true;
+		};
+	}
+
+	if (window.jivo_onClose) {
+		window.jivo_onClose = () => {
+			isChatOpen = false;
+		};
+	}
+
+	if (window.jivo_destroy) {
+		window.jivo_destroy();
+	}
 });
+
+function toggleChat() {
+	if (isChatOpen) {
+		closeChat();
+	} else {
+		openChat();
+	}
+}
+
+function openChat() {
+	openJivositeChat();
+	// openLiveChat();
+
+	isChatOpen = true;
+}
+
+function openJivositeChat() {
+	if (window.jivo_api) {
+		window.jivo_api.open({});
+	}
+}
+
+function closeChat() {
+	if (window.jivo_api) {
+		window.jivo_api.close();
+	}
+
+	isChatOpen = false;
+}
 
 function openLiveChat() {
 	if (typeof window.LC_API !== 'undefined') {
+		// window.LiveChatWidget.call('set_language', 'ru');
+
+		var liveChatScript = document.createElement('script');
+		liveChatScript.src = 'https://cdn.livechatinc.com/widget.js';
+		liveChatScript.async = true;
+		liveChatScript.onload = function () {
+			window.LiveChatWidget.call('set_language', 'ru');
+		};
+		document.head.appendChild(liveChatScript);
+
 		window.LC_API.open_chat_window();
-		isChatOpen = true;
 	}
 }
 </script>
 
 <button
-	on:click="{openLiveChat}"
+	on:click="{toggleChat}"
 	class="flex h-auto w-full flex-col justify-center rounded-lg
     {isChatOpen ? 'bg-activeElements' : 'bg-[#363A45]'}">
 	<div class="flex h-[60px] items-center rounded-lg text-gray-500 no-underline transition duration-300 hover:cursor-pointer">
