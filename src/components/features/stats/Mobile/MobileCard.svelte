9c3423fe6030de7e32c34bf10b6b1e4ac64befbd
Magomedsoltan Kasumov <77916046+kenzWeb@users.noreply.github.com>
2024-12-24 13:48:21 +0300

    mobile card

---
+++ src/components/features/stats/Mobile/MobileCard.svelte
@@ -0,0 +1,50 @@
+<script>
+import Button from '$src/components/ui/button/button.svelte';
+
+
+</script>
+
+
+<div class="bg-[#363A45] rounded-[20px] text-white p-4">
+	<div class="flex items-center justify-between text-[20px]">
+		<div class="">
+			<img class="w-[73px] mb-3" src="/icons/bk/winline-big.svg" alt="">
+			<h2>api.name</h2>
+		</div>
+
+		<div>
+			<h3>api.id</h3>
+		</div>
+	</div>
+
+	<div class="grid grid-cols-2 gap-1 mt-4 flex-1 mb-4">
+		<div class="flex flex-col items-center justify-between gap-1">
+			<div class="bg-[#171B26] rounded-[16px] flex flex-col p-4 w-full">
+				<h4>api.balance</h4>
+				<p>api.balanceCount</p>
+			</div>
+			<div class="bg-[#171B26] rounded-[16px] flex flex-col p-4 w-full">
+				<h4>api.sum</h4>
+				<p>api.sumCount</p>
+			</div>
+		</div>
+		<div class="flex flex-col items-center justify-between">
+			<div class="bg-[#171B26] rounded-[16px] flex flex-col p-4 w-full">
+				<h4>api.win</h4>
+				<p>api.winCount</p>
+			</div>
+			<div class="bg-[#171B26] rounded-[16px] flex flex-col p-4 w-full">
+				<h4>api.bets</h4>
+				<p>api.betsCount</p>
+			</div>
+		</div>
+	</div>
+
+
+<Button class="h-[56px] text-[16px]" variant='card'>Подробнее</Button>
+	
+</div>
+
+<style>
+
+</style>
---
+++ src/components/features/stats/Mobile/MobileFilterButton.svelte
@@ -0,0 +1,20 @@
+<script>
+import Button from '$src/components/ui/button/button.svelte';
+import { toggleSidebar } from '$src/utils/functions/toggleSidebar';
+</script>
+
+<div class="button">
+
+	<Button variant='interactive' on:click={toggleSidebar}>
+		<img src="/icons/filter.svg" alt="">
+	</Button>
+</div>
+
+
+<style lang="scss">
+	@media (min-width: 650px) {
+		.button {
+			display: none;
+		}
+	}
+</style>
