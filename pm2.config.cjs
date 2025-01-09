1d50e02fb2a63aa666e4fc9d35ef79c2fdbd473e
nkz9 <nkz9>
2025-01-10 01:15:27 +0300

    prod

--- pm2.config.cjs
+++ pm2.config.cjs
@@ -5,7 +5,6 @@ module.exports = {
 			script: 'index.js',
 			watch: true,
 			env: {
-				SERVER_URL: 'http://https://dev-api-gateway-v1.sntmq.1keep.bet:11250/api',
 				PORT: 4671,
 				NODE_ENV: 'production'
 			}
