d5ef183145ba834e81680aae4f426d19a3eef200
Magomedsoltan Kasumov <77916046+kenzWeb@users.noreply.github.com>
2025-02-23 19:15:15 +0300

    feat: update columns

--- src/components/widgets/stats/BetsTable/columns.ts
+++ src/components/widgets/stats/BetsTable/columns.ts
@@ -1,4 +1,5 @@
 import { renderComponent } from '$src/components/ui/data-table'
+import { formatDate } from '$src/utils/functions/formatDate'
 import { formatNumber } from '$src/utils/functions/formatNumber'
 
 import SortableHeader from '../../table/SortableHeader.svelte'
@@ -23,7 +24,6 @@ export const getColumns = (t: (key: string, params?: Record<string, any>) => str
 				onClick: () => column.toggleSorting(column.getIsSorted() === 'asc'),
 				isSorted: column.getIsSorted()
 			}),
-		meta: { textAlign: 'left' } as BetColumnMeta,
 		cell: ({ row }) => {
 			try {
 				const date = new Date(row.original.dates.placed);
@@ -35,14 +35,35 @@ export const getColumns = (t: (key: string, params?: Record<string, any>) => str
 		}
 	},
 	{
+		accessorKey: 'clientSeq',
+		header: ({ column }) =>
+			renderComponent(SortableHeader, {
+				title: 'ID',
+				onClick: () => column.toggleSorting(column.getIsSorted() === 'asc'),
+				isSorted: column.getIsSorted(),
+				textAlign: 'right'
+			}),
+		meta: { textAlign: 'right' } as BetColumnMeta
+	},
+	{
+		accessorKey: 'extendedId',
+		header: ({ column }) =>
+			renderComponent(SortableHeader, {
+				title: t('columns.bet.bkId'),
+				onClick: () => column.toggleSorting(column.getIsSorted() === 'asc'),
+				isSorted: column.getIsSorted(),
+				textAlign: 'right'
+			}),
+		meta: { textAlign: 'right' } as BetColumnMeta
+	},
+	{
 		accessorKey: 'siteName',
 		header: ({ column }) =>
 			renderComponent(SortableHeader, {
 				title: t('columns.bet.bookmaker'),
 				onClick: () => column.toggleSorting(column.getIsSorted() === 'asc'),
 				isSorted: column.getIsSorted()
-			}),
-		meta: { textAlign: 'left' } as BetColumnMeta
+			})
 	},
 	{
 		accessorKey: 'event.sport',
@@ -52,6 +73,42 @@ export const getColumns = (t: (key: string, params?: Record<string, any>) => str
 				onClick: () => column.toggleSorting(column.getIsSorted() === 'asc'),
 				isSorted: column.getIsSorted()
 			})
+	},
+	{
+		accessorKey: 'event.startTime',
+		header: ({ column }) =>
+			renderComponent(SortableHeader, {
+				title: t('columns.bet.startTime'),
+				onClick: () => column.toggleSorting(column.getIsSorted() === 'asc'),
+				isSorted: column.getIsSorted()
+			}),
+		cell: ({ row }) => {
+			try {
+				return row.original.event.startTime ? formatDate(row.original.event.startTime) : '';
+			} catch (e) {
+				return '';
+			}
+		}
+	},
+	{
+		accessorKey: 'isLive',
+		header: ({ column }) =>
+			renderComponent(SortableHeader, {
+				title: t('columns.bet.type'),
+				onClick: () => column.toggleSorting(column.getIsSorted() === 'asc'),
+				isSorted: column.getIsSorted()
+			}),
+		cell: ({ row }) => (row.original.isLive ? 'Live' : 'Prematch')
+	},
+	{
+		accessorKey: 'event.competitionName',
+		header: ({ column }) =>
+			renderComponent(SortableHeader, {
+				title: t('columns.bet.tournament'),
+				onClick: () => column.toggleSorting(column.getIsSorted() === 'asc'),
+				isSorted: column.getIsSorted()
+			}),
+		cell: ({ row }) => row.original.event.competitionName?.default || ''
 	},
 	{
 		accessorKey: 'event',
@@ -72,29 +129,6 @@ export const getColumns = (t: (key: string, params?: Record<string, any>) => str
 		}
 	},
 	{
-		accessorKey: 'amounts.stake',
-		header: ({ column }) =>
-			renderComponent(SortableHeader, {
-				title: t('columns.bet.ammount'),
-				onClick: () => column.toggleSorting(column.getIsSorted() === 'asc'),
-				isSorted: column.getIsSorted(),
-				textAlign: 'right'
-			}),
-		cell: ({ row }) => formatNumber(Number(row.original.amounts.stake)),
-		meta: { textAlign: 'right' } as BetColumnMeta
-	},
-	{
-		accessorKey: 'outcome.default',
-		header: ({ column }) =>
-			renderComponent(SortableHeader, {
-				title: t('stats.outcome'),
-				onClick: () => column.toggleSorting(column.getIsSorted() === 'asc'),
-				isSorted: column.getIsSorted(),
-				textAlign: 'left'
-			}),
-		meta: { textAlign: 'left' } as BetColumnMeta
-	},
-	{
 		accessorKey: 'rate',
 		header: ({ column }) =>
 			renderComponent(SortableHeader, {
@@ -104,6 +138,27 @@ export const getColumns = (t: (key: string, params?: Record<string, any>) => str
 				textAlign: 'right'
 			}),
 		cell: ({ row }) => formatNumber(Number(row.original.rate)),
+		meta: { textAlign: 'right' } as BetColumnMeta
+	},
+	{
+		accessorKey: 'status',
+		header: ({ column }) =>
+			renderComponent(SortableHeader, {
+				title: t('columns.bet.status'),
+				onClick: () => column.toggleSorting(column.getIsSorted() === 'asc'),
+				isSorted: column.getIsSorted()
+			})
+	},
+	{
+		accessorKey: 'amounts.stake',
+		header: ({ column }) =>
+			renderComponent(SortableHeader, {
+				title: t('columns.bet.amount'),
+				onClick: () => column.toggleSorting(column.getIsSorted() === 'asc'),
+				isSorted: column.getIsSorted(),
+				textAlign: 'right'
+			}),
+		cell: ({ row }) => formatNumber(Number(row.original.amounts.stake)),
 		meta: { textAlign: 'right' } as BetColumnMeta
 	},
 	{
@@ -119,21 +174,29 @@ export const getColumns = (t: (key: string, params?: Record<string, any>) => str
 		meta: { textAlign: 'right' } as BetColumnMeta
 	},
 	{
-		accessorKey: 'status',
+		accessorKey: 'profit',
 		header: ({ column }) =>
 			renderComponent(SortableHeader, {
-				title: t('columns.bet.status'),
+				title: t('columns.bet.profit'),
 				onClick: () => column.toggleSorting(column.getIsSorted() === 'asc'),
-				isSorted: column.getIsSorted()
-			})
-	}
+				isSorted: column.getIsSorted(),
+				textAlign: 'right'
+			}),
+		cell: ({ row }) => {
+			const win = Number(row.original.amounts.win);
+			const stake = Number(row.original.amounts.stake);
+			return formatNumber(win - stake);
+		},
+		meta: { textAlign: 'right' } as BetColumnMeta
+	},
+	// {
+	// 	accessorKey: 'score',
+	// 	header: ({ column }) =>
+	// 		renderComponent(SortableHeader, {
+	// 			title: t('columns.bet.score'),
+	// 			onClick: () => column.toggleSorting(column.getIsSorted() === 'asc'),
+	// 			isSorted: column.getIsSorted()
+	// 		}),
+	// 	cell: ({ row }) => row.original.score || ''
+	// }
 ];
-
-function getNestedValue(obj: any, path: string, defaultValue: any = '') {
-	try {
-		return path.split('.').reduce((current, key) => current[key], obj) ?? defaultValue;
-	} catch (e) {
-		console.error(`Error accessing path ${path}:`, e);
-		return defaultValue;
-	}
-}
